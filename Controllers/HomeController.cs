5560cefb6352177d5a9d307f869d745c1d784058
Manuel Pinheiro <manuel.pinheiro@libero.it>
2024-07-26 10:02:37 +0200

    Api

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,28 +23,7 @@ namespace Progetto_Settimana_2_Manuel.Controllers
             return View();
         }
 
-        [HttpPost]
-        public IActionResult SearchByCodiceFiscale(string codiceFiscale)
-        {
-            var cliente = _clienteDAO.GetAll().FirstOrDefault(c => c.CodiceFiscale == codiceFiscale);
-            if (cliente == null)
-            {
-                ViewBag.ErrorMessage = "Cliente non trovato";
-                return View("Index");
-            }
-
-            var prenotazioni = _prenotazioneDAO.GetAll().Where(p => p.CodiceFiscaleCliente == cliente.ID).ToList();
-            ViewBag.Prenotazioni = prenotazioni;
-            return View("Index");
-        }
-
-        [HttpGet]
-        public IActionResult CountPensioneCompleta()
-        {
-            var count = _prenotazioneDAO.GetAll().Count(p => p.TipoSoggiorno == "pensione completa");
-            ViewBag.CountPensioneCompleta = count;
-            return View("Index");
-        }
+        
 
         public IActionResult Privacy()
         {
---
+++ Controllers/PrenotazioniApiController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Progetto_Settimana_2_Manuel.DAO;
+using Progetto_Settimana_2_Manuel.Models;
+using System.Linq;
+
+namespace Progetto_Settimana_2_Manuel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrenotazioniApiController : ControllerBase
+    {
+        private readonly IPrenotazioneDAO _prenotazioneDAO;
+        private readonly IClienteDAO _clienteDAO;
+
+        public PrenotazioniApiController(IPrenotazioneDAO prenotazioneDAO, IClienteDAO clienteDAO)
+        {
+            _prenotazioneDAO = prenotazioneDAO;
+            _clienteDAO = clienteDAO;
+        }
+
+        [HttpGet("SearchByCodiceFiscale")]
+        public IActionResult SearchByCodiceFiscale(string codiceFiscale)
+        {
+            var cliente = _clienteDAO.GetAll().FirstOrDefault(c => c.CodiceFiscale == codiceFiscale);
+            if (cliente == null)
+            {
+                return NotFound(new { Message = "Cliente non trovato" });
+            }
+
+            var prenotazioni = _prenotazioneDAO.GetAll().Where(p => p.CodiceFiscaleCliente == cliente.ID).ToList();
+            return Ok(prenotazioni);
+        }
+
+
+        [HttpGet("CountPensioneCompleta")]
+        public IActionResult CountPensioneCompleta()
+        {
+            var prenotazioni = _prenotazioneDAO.GetAll().Where(p => p.TipoSoggiorno == "pensione completa").ToList();
+            var count = prenotazioni.Count;
+            var clienti = prenotazioni.Select(p => _clienteDAO.GetById(p.CodiceFiscaleCliente))
+                                       .Distinct()
+                                       .Select(c => new { c.Nome, c.Cognome, c.CodiceFiscale })
+                                       .ToList();
+
+            return Ok(new { Count = count, Clienti = clienti });
+        }
+    }
+}
